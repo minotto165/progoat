df3a7fcc6dbd80f811f50a15a74a3348289bf065
minotto <minotto115@gmail.com>
2026-02-28 17:55:59 +0900

    mdレンダリングのコードを追加

--- cmd/start.go
+++ cmd/start.go
@@ -4,10 +4,12 @@ Copyright © 2026 minotto
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 
+	"github.com/charmbracelet/glamour"
 	"github.com/spf13/cobra"
 )
 
@@ -20,38 +22,81 @@ Read the slides, write your code, and get feedback from the AI judge.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		courseID := args[0]
-		coursePath, err := getCoursePath(courseID)
+
+		course, err := getCourseStruct(courseID)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
+		coursePath := filepath.Join(coursesDir, course.Title)
+
 		fmt.Println("[INFO] Course Directory:", coursePath)
+
+		for _, c := range course.Lessons {
+			slides := c.Slides
+			for _, s := range slides {
+				out, err := glamour.Render(s, "dark")
+				if err != nil {
+					fmt.Println("Error:", err)
+					return
+				}
+				fmt.Println(out)
+				fmt.Println("----------------------")
+			}
+		}
 
 	},
 }
 
-func getCoursePath(courseID string) (string, error) {
+func getCourses() ([]Course, error) {
 	files, err := os.ReadDir(coursesDir)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-
-	var path string
-
+	var courses []Course
 	for _, file := range files {
 		if file.IsDir() {
 			dirName := file.Name()
-			if dirName == courseID {
-				path = filepath.Join(coursesDir, dirName)
-				break
+			coursesJsonPath := filepath.Join(coursesDir, dirName, "course.json")
+			coursesJson, err := os.ReadFile(coursesJsonPath)
+			if err != nil {
+				return nil, err
 			}
+			// Convert to struct
+			var course Course
+			err = json.Unmarshal([]byte(coursesJson), &course)
+			if err != nil {
+				return nil, fmt.Errorf("Error parsing JSON: %s", err)
+			}
+
+			// Add to slice
+			courses = append(courses, course)
 		}
 	}
-	if path == "" {
-		return "", fmt.Errorf("No such a course: %s", courseID)
+	return courses, nil
+}
+
+func getCourseStruct(courseID string) (Course, error) {
+	courses, err := getCourses()
+	course := Course{}
+	found := 0
+	if err != nil {
+		return course, err
 	}
 
-	return path, nil
+	for _, c := range courses {
+		ID := c.ID
+		if ID == courseID {
+			course = c
+			found = 1
+			break
+		}
+	}
+	if found == 0 {
+		return course, fmt.Errorf("No such a course: %s", courseID)
+	}
+
+	return course, nil
 }
 
 func init() {
