b13c247642a77735b78f8a724a3a74e5dfb148f7
minotto <minotto115@gmail.com>
2026-03-07 01:56:19 +0900

    Course,Lesson構造体にisCompletedを追加(#14)

--- internal/course/course.go
+++ internal/course/course.go
@@ -13,6 +13,7 @@ type Course struct {
 	Description         string   `json:"description"`
 	ProgrammingLanguage string   `json:"programming_language"`
 	Lessons             []Lesson `json:"lessons"`
+	IsCompleted         bool     `json:"is_completed"`
 }
 
 type Lesson struct {
@@ -23,6 +24,7 @@ type Lesson struct {
 	InitialCode     string   `json:"initial_code"`
 	CorrectOutput   string   `json:"correct_output"`
 	FileName        string   `json:"file_name"`
+	IsCompleted     bool     `json:"is_completed"`
 }
 
 func GetCourses(coursesPath string) ([]Course, error) {
